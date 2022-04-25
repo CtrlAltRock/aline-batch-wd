b388c09ada8d12f26f7ddb3b8480835b69ba59b0
WillemDuiker <Willem.Duiker@smoothstack.com>
2022-04-25 10:03:23 -0500

    NRVNA - 135 - Master - Initial commit

---
+++ src/main/java/com/alinebatch/alinebatchwd/AlineBatchWdApplication.java
@@ -0,0 +1,13 @@
+package com.alinebatch.alinebatchwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlineBatchWdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlineBatchWdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alinebatch/alinebatchwd/AlineBatchWdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinebatch.alinebatchwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlineBatchWdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
